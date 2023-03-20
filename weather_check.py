c6dc925d07219bb6fe0bf232f05acd93d9995784
Sahadeva Gowda <sahadevamk121@gmail.com>
2023-03-21 00:13:04 +0530

    Create weather_check.py

---
+++ weather_check.py
@@ -0,0 +1,12 @@
+temp = int(input("Enter the temperature in celcius>> "))
+if temp > 35:
+    print("It's hot!...")
+    print("Drink more water")
+
+elif temp < 25:
+    print("It's cold!..")
+    print("Please wear sweaters")
+
+else:
+    print("It's Normal")
+    print("Have a nice day")
