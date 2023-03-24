7ae8229251e8be56f78ca11d25a1e7543227d897
Sahadeva Gowda <sahadevamk121@gmail.com>
2023-03-24 21:28:16 +0530

    Create positivenegativecheck.py

---
+++ positivenegativecheck.py
@@ -0,0 +1,8 @@
+## check whether the given number is positive or negative
+
+x = int (input('enter the number '))
+
+if (x>=0):
+    print(f"the given number is positive")
+else:
+    print(f"the given number is negative")
