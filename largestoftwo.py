b852946ad5b500dde5c0be7c838919fd5b8adcee
Sahadeva Gowda <sahadevamk121@gmail.com>
2023-03-24 21:33:58 +0530

    Create largestoftwo.py

---
+++ largestoftwo.py
@@ -0,0 +1,8 @@
+## largest of two numbers
+
+x=int(input('enter the first number'))
+y=int(input('enter the second number'))
+if (x>y):
+    print( f"first num is greater than second number")
+else:
+    print(f"second number is greater than first number")
