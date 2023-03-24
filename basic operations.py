5cbbafa4a9d595019864752c9022ea6f519bbe88
Sahadeva Gowda <sahadevamk121@gmail.com>
2023-03-24 20:55:49 +0530

    Create basic operations.py

---
+++ basic operations.py
@@ -0,0 +1,14 @@
+## accept two numbers and perform basic arthematic operations
+
+x=int(input('enter the num'))
+y=int(input('enter the second num'))
+
+summ = x+y
+sub = x-y
+mul = x*y
+div = x//y
+print(summ)
+print(sub)
+print(mul)
+print(div)
+
