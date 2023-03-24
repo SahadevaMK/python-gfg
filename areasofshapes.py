f6327f108c2de8494d5116a395ca8bd3e67b69e9
Sahadeva Gowda <sahadevamk121@gmail.com>
2023-03-24 21:04:37 +0530

    Create areasofshapes.py

---
+++ areasofshapes.py
@@ -0,0 +1,13 @@
+## area of geometrical shapes
+
+x = int(input('enter the radius of circle'))
+y= int(input('enter the hieght of triangle'))
+z = int (input('enter the breadth of triangle'))
+l= int(input('enter the hieght of rectangle'))
+b = int (input('enter the breadth of rectangle'))
+area = 3.14*x**2
+areat = y*z/2
+arear = l*b
+print(f"area of circle is ", area)
+print(areat)
+print(arear)
