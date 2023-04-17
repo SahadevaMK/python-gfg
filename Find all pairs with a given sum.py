74c70bdfd67c35a7832a15acaddbfac63e902b31
Sahadeva Gowda <sahadevamk121@gmail.com>
2023-04-17 23:37:32 +0530

    Create Find all pairs with a given sum.py

---
+++ Find all pairs with a given sum.py
@@ -0,0 +1,9 @@
+class Solution:
+    def allPairs(self, A, B, N, M, X):
+        li = []
+        # Your code goes here 
+        for i in range(len(A)):
+            for j in range(len(B)):
+                if (A[i]+B[j]) == X:
+                    li.append((A[i],B[j]))
+        return sorted(li)
