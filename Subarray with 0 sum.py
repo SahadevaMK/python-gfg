fb47bb70236d5b7c40af77a8fe77841195a52987
Sahadeva Gowda <sahadevamk121@gmail.com>
2023-06-02 02:36:41 +0530

    Create Subarray with 0 sum.py

---
+++ Subarray with 0 sum.py
@@ -0,0 +1,15 @@
+class Solution:
+    
+    #Function to check whether there is a subarray present with 0-sum or not.
+    def subArrayExists(self,arr,n):
+        ##Your code here
+        #Return true or false
+        s = set() 
+     
+        sum = 0
+        for i in range(n): 
+            sum += arr[i] 
+            if sum == 0 or sum in s: 
+                return True
+            s.add(sum) 
+        return False
